d0124b09828e8d672149d7e6fa06d0a2f6ab2d90
Brev860 <60907093+Brev860@users.noreply.github.com>
2020-06-06 19:22:40 -0400

    Add files via upload

---
+++ Week 1_assignment/my-app/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>Complete Week 1 Reading and Pause/Practive Videos</p>
+      Completed: <input type="checkbox"/>
+
+      <p>Complete Week 1 Slack</p>
+      Completed: <input type="checkbox"/>
+
+      <p>Complete Week 1 LearnIts</p>
+      Completed: <input type="checkbox"/>
+
+      <p>Complete Week 1 Project</p>
+      Completed: <input type="checkbox"/>
+      
+    </div>
+  );
+}
+
+export default App;
